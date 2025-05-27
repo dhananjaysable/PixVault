d97647d1f6c5e3b1fa6e08fa2bc08f2ec90ed9b0
Dhananjay Sable <dhananjaysable370@gmail.com>
2025-05-27 21:50:04 +0530

    save

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(cors({
-    origin: ["http://localhost:5173", "https://pix-vault.netlify.app"],
+    origin: ["http://localhost:5173", "https://pix-vault.netlify.app", "https://my-pix-vault.netlify.app"],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
